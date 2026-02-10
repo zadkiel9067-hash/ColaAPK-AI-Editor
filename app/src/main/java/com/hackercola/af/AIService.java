bb42ac988e0fe9618a018b8b26e33547fe58d791
Enayatullah Haqqani <zadkiel9067@gmail.com>
2026-02-10 17:19:09 +0330

    Add AIService class with sendCommand method

---
+++ app/src/main/java/com/hackercola/af/AIService.java
@@ -0,0 +1,8 @@
+package com.hackercola.af;
+
+public class AIService {
+    public static String sendCommand(String prompt) {
+        // بعداً API واقعی اینجا وصل میشه
+        return "{ \"action\": \"none\" }";
+    }
+}
